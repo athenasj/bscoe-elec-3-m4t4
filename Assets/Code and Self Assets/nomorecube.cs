5767271ae6f7623e6439a3308643e9ce50e6483d
athenasj <30370786+athenasj@users.noreply.github.com>
2018-02-25 17:29:55 +0800

    Added explosion of enemy ships.

---
+++ Assets/Code and Self Assets/nomorecube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nomorecube : MonoBehaviour {
+
+    public GameObject explosive;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnParticleCollision(GameObject other)
+    {
+        
+       
+        if (other.gameObject.tag == "EnemyShip")
+        {
+            //debug
+            print("boomboom");
+            print(other);
+
+            //instantiate explosion
+            Instantiate(explosive, other.transform.position, other.transform.rotation);
+            Destroy(other);
+            Destroy(other.transform.parent.gameObject);
+        }
+    }
+
+    
+
+}
