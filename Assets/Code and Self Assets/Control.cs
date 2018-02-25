50d3d3e091a6b28395f07014cf0b5e5ed76e9049
athenasj <30370786+athenasj@users.noreply.github.com>
2018-02-25 10:45:17 +0800

    Added obstacles.
    Refactored Control. Created trigger for collision script. Arranged particle for collision. TO DO: On collision, activate child particle. Add enemy ship. Particle collision for laser.

--- Assets/Code and Self Assets/Control.cs
+++ Assets/Code and Self Assets/Control.cs
@@ -11,10 +11,10 @@ public class Control : MonoBehaviour
     Vector3 tempPos, tempRot;
     float speed = 2f;
     float zed = 3.72f;
-    public float xMin = -0.6940987f;
-    public float xMax = 0.3849159f;
-    public float yMin = -0.5663472f;
-    public float yMax = 0.2940772f;
+    public float xMin = -2f;
+    public float xMax = 2.14f;
+    public float yMin = -1.81f;
+    public float yMax = 1.74f;
     public float tilt = 10f;
     public float yRotation = 0.0F;
     public float xRotation = 0.0F;
@@ -27,8 +27,25 @@ public class Control : MonoBehaviour
 
     }
 
-    void smoothRotateX()
+    void checkInput()
     {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+    }
+
+    void assignUpDown()
+    {
+        tempPos.x += xThrow * speed * Time.deltaTime;
+        tempPos.y += yThrow * speed * Time.deltaTime;
+        tempPos = new Vector3(
+             Mathf.Clamp(tempPos.x, xMin, xMax),
+             Mathf.Clamp(tempPos.y, yMin, yMax),
+             transform.localPosition.z
+             );
+    }
+
+    void smoothRotateX()
+    {   //the name is X because the rotation is different from the x and y axis for transform this is actually z
         if (yThrow == 0 && (transform.localEulerAngles.x >= 1 || transform.localEulerAngles.x <= -1))
         {   //-5 31
 
@@ -45,38 +62,8 @@ public class Control : MonoBehaviour
         yRotation = Mathf.Clamp(yRotation, -30.0f, 30.0f);
     }
 
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        tempPos = transform.localPosition;
-        /*   INPUT   */
-        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-
-        //print(xThrow);
-        print(yThrow);
-        //print("X: "+tempPos.x);
-
-        //print("Y: "+tempPos.y);
-
-        /*  MOVEMENT FOR UP AND DOWN  */
-        tempPos.x += xThrow * speed * Time.deltaTime;
-        tempPos.y += yThrow * speed * Time.deltaTime;
-        tempPos = new Vector3(
-             Mathf.Clamp(tempPos.x, xMin, xMax),
-             Mathf.Clamp(tempPos.y, yMin, yMax),
-             transform.localPosition.z
-             );
-
-
-
-        yRotation = Mathf.Clamp(yRotation, -30.0f, 30.0f);
-        xRotation = Mathf.Clamp(xRotation, -18.0f, 20.0f);
-        /*  FOR ROTATION  */
-        smoothRotateX();
-
+    void smoothRotateY()
+    {   //the name is Y because the rotation is different from the x and y axis for transform this is actually x but input is from y
         if (xThrow == 0 && (transform.localEulerAngles.z >= 1 || transform.localEulerAngles.z <= -1))
         {   //-5 31
 
@@ -90,21 +77,51 @@ public class Control : MonoBehaviour
             xRotation = 0f;
         }
         xRotation += -xThrow * tilt * Time.deltaTime * 10;
+    }
+
+
+
+    
+    void debug()
+    {
+        print(xThrow);
+        print(yThrow);
+        print("X: "+tempPos.x);
+        print("Y: "+tempPos.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPos = transform.localPosition;
+        /*   INPUT   */
+        checkInput();
+        //debug();
+        /*  MOVEMENT FOR UP AND DOWN  */
+        assignUpDown();
+        
+        /*  FOR ROTATION  */
+        smoothRotateX();
+        smoothRotateY();
+
+        /* Clamping x and y rotation values */
+        yRotation = Mathf.Clamp(yRotation, -30.0f, 30.0f);
         xRotation = Mathf.Clamp(xRotation, -18.0f, 20.0f);
 
-
-        //print(yRotation);
+        /* Applying of rotation values */
         transform.localEulerAngles = new Vector3(
-            Mathf.Clamp(yRotation, -30.0f, 30.0f),
+            yRotation,
             0,
-            Mathf.Clamp(xRotation, -20.0f, 20.0f)
+            xRotation
             );
 
-
-
-        //apply movement
-        if(transform.localEulerAngles.z >= 4 || transform.localEulerAngles.z <= -4 || transform.localEulerAngles.x >= 4 || transform.localEulerAngles.x <= -4 )
+        /* apply movement - only apply transform when newposi is not less or more than 4*/
+        if (transform.localEulerAngles.z >= 4 || transform.localEulerAngles.z <= -4 || transform.localEulerAngles.x >= 4 || transform.localEulerAngles.x <= -4)
+        {
             transform.localPosition = tempPos;
+        }
 
     }
+    
+
 }
